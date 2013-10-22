3479f13b1a67b65b0b9854f23a47e82a65d808ce
alexgo <alex@gounares.com>
2013-10-22 15:19:00 -0700

    latest fixes

--- test/simple.js
+++ test/simple.js
@@ -113,7 +113,6 @@ describe('basic wrapping test', function(){
       exportTest.b.__concurix_wrapper_for__.should.equal('b');
       exportTest.b();
       exportTest.b();
-      console.log('cache ', global.concurix.traceAggregate.linkCache);
       global.concurix.traceAggregate.nodeCache[id].num_calls.should.equal(2);
       global.concurix.traceAggregate.nodeCache[id].duration.should.not.be.NaN;
       global.concurix.traceAggregate.nodeCache[id].mem_delta.should.not.be.NaN;
@@ -127,7 +126,44 @@ describe('basic wrapping test', function(){
       linkCache[keys[0]].num_calls.should.equal(3);
       linkCache[keys[0]].total_delay.should.not.be.NaN;  
     });
-  });  
+  }); 
+  
+  describe('callback test', function(){
+    // simple test objects
+    function callback(arg){ console.log("CALLBACK!!!"); return arg;}
+    
+    var exportTest = {
+      a: function a(arg1, cb ){ console.log("called a", cb); return cb(arg1 + arg1);},
+      b: function b(arg1){ console.log("trying to call b", callback); return this.a(arg1, callback);},
+      c: "hello"
+    };
+    exportTest.b(1);
+    var id = null;
+    function beforeHook(trace, clientState){
+      id = trace.funInfo.id;
+    }
+    it('count for a should be 2', function(){
+      mstats.reset();
+      mstats.wrap("test", exportTest, {beforeHook: beforeHook});
+      exportTest.a.__concurix_wrapper_for__.should.equal('a');
+      exportTest.b.__concurix_wrapper_for__.should.equal('b');
+      exportTest.b(1);
+      exportTest.b(2);
+      //console.log('cache ', global.concurix.traceAggregate.linkCache);
+      global.concurix.traceAggregate.nodeCache[id].num_calls.should.equal(2);
+      global.concurix.traceAggregate.nodeCache[id].duration.should.not.be.NaN;
+      global.concurix.traceAggregate.nodeCache[id].mem_delta.should.not.be.NaN;
+      exportTest.b(3);
+      global.concurix.traceAggregate.nodeCache[id].num_calls.should.equal(3); 
+      
+      //now test the link cache
+      var linkCache = global.concurix.traceAggregate.linkCache;
+      var keys = Object.keys(linkCache);
+      keys.length.should.equal(2);
+      linkCache[keys[0]].num_calls.should.equal(3);
+      linkCache[keys[0]].total_delay.should.not.be.NaN;  
+    });
+  }); 
        
 });
   
